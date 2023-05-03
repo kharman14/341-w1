e50197062b375d34cbe5d2974788c629517aa358
kharman14 <harman.katelyn96@gmail.com>
2023-05-03 12:55:00 -0600

    personal assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+const cors = require('cors'); // Make sure to require the cors package
+
+app.use(cors({
+  origin: ['http://localhost:8080', 'http://127.0.0.1:5500'], // Add the new origin
+}));
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
